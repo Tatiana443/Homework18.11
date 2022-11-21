dce8fb2e655d44997f66ea3910df9d0e02b815db
Tatiana443 <tvb399@yandex.ru>
2022-11-21 17:40:15 +0600

    Project Task19

---
+++ Task19/Program.cs
@@ -0,0 +1,20 @@
+﻿int num, rem, sum = 0, temp;
+Console.WriteLine("Enter a number: ");
+num = Convert.ToInt32(Console.ReadLine());
+temp = num;
+if (num < 0) Console.WriteLine("A palindrom cannot be a negativ number");
+while (num > 0)
+{
+    rem = num % 10;  
+    num = num / 10; 
+    sum = sum * 10 + rem; 
+}
+if (temp == sum) 
+{
+    Console.WriteLine("Number is palindrome");
+}
+else 
+{
+    Console.WriteLine("Number is not a palindrome");
+}
+    
