d229fc30e8b6b4d23eac60407b29603e08007fe1
Tatiana443 <tvb399@yandex.ru>
2022-11-22 12:05:15 +0600

    Project Task23

---
+++ Task23/Program.cs
@@ -0,0 +1,18 @@
+﻿int n = -1;
+while (n < 1)
+{
+    Console.WriteLine("Enter number: ");
+    n = Convert.ToInt32(Console.ReadLine());
+    if (n < 1) Console.WriteLine("Incorrect data");
+}
+
+void Cube(int num)
+{
+    int count = 1;
+    while (count <= num)
+    {
+        Console.WriteLine($"{count} {count * count * count}");
+        count ++;
+    }
+}
+Cube(n);
